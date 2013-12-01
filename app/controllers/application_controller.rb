85b79176970dba61d701607bd3e5397cfb60a64a
Drew MacInnis <drewmacinnis@gmail.com>
2013-12-01 12:57:00 -0500

    Attempt to add iphone support.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :format_for_iphone
+
+  def format_for_iphone
+	if request.env["HTTP_USER_AGENT"] and request.env["HTTP_USER_AGENT"][/(Mobile\/.+Safari)/]
+	  request.format = :iphone
+	end
+  end
 end
