75eddce5d6c9f31a1cbf7d25a0aad2505ccde766
Drew MacInnis <drewmacinnis@gmail.com>
2013-12-01 13:05:25 -0500

    Prepend mobile views rather than use iphone mime type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :format_for_iphone
+  before_filter :format_for_mobile
 
-  def format_for_iphone
+  def format_for_mobile
 	if request.env["HTTP_USER_AGENT"] and request.env["HTTP_USER_AGENT"][/(Mobile\/.+Safari)/]
-	  request.format = :iphone
+#	  request.format = :iphone
+	  prepend_view_path "app/views/mobile"
 	end
   end
 end
